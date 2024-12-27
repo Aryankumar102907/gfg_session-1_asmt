22ea2c45eef4cb00c7407279b82f65bd6a696648
Aryan Kumar <155047504+Aryankumar102907@users.noreply.github.com>
2024-12-27 16:30:59 +0000

    Done3

--- day1/assignment3.js
+++ day1/assignment3.js
@@ -10,7 +10,17 @@ Example: modifyObject({a: 5, b: 15, c: 8}) should return {a: 10, b: 5, c: 13}
 
 function modifyObject(obj) {
     //Write your code here.
-    return result;
+    for (let prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            if (obj[prop] < 10) {
+                obj[prop] += 5;
+            }
+            else{
+                obj[prop] -= 10;
+            }
+        }
+    }
+    return obj;
 }
 
 module.exports = { modifyObject };
